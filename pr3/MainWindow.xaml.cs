10d7a88ad35a78ba2848a06d78c550bd2eb2b230
Osennikov Evgeniy <sbravl637@gmail.com>
2025-09-15 13:42:53 +0500

    Выполненная практическая работа.Осенников.

--- pr3/MainWindow.xaml.cs
+++ pr3/MainWindow.xaml.cs
@@ -23,19 +23,22 @@ namespace pr3
     {   
         
         public Classes.PersonInfo Player = new Classes.PersonInfo("Student",100,10,1,0,0,5);
-        public List<Classes.PersonInfo> Empty = new List<Classes.PersonInfo>();
+        public Classes.PersonInfo Empty;
+        public List<Classes.PersonInfo> Emptys = new List<Classes.PersonInfo>();
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
             UserInfoPlayer();
-            Empty.Add(new Classes.PersonInfo("Враг 1", 100, 20, 1, 15, 5, 20));
-            Empty.Add(new Classes.PersonInfo("Враг 2", 20, 5, 1, 5, 2, 5));
-            Empty.Add(new Classes.PersonInfo("Враг 3", 50, 3, 1, 10, 10, 15));
+            Emptys.Add(new Classes.PersonInfo("Враг 1", 100, 20, 1, 15, 5, 20));
+            Emptys.Add(new Classes.PersonInfo("Враг 2", 20, 5, 1, 5, 2, 5));
+            Emptys.Add(new Classes.PersonInfo("Враг 3", 50, 3, 1, 10, 10, 15));
 
             dispatcherTimer.Tick += AttackPlayer;
             dispatcherTimer.Interval = new System.TimeSpan(0, 0, 10);
             dispatcherTimer.Start();
+
+            SelectEmpty();
         }
 
         public void UserInfoPlayer()
@@ -55,14 +58,40 @@ namespace pr3
             playerMoney.Content = "Монеты: " + Player.Money;
         }
 
+        public void SelectEmpty()
+        {
+            int Id = new Random().Next(0,Emptys.Count);
+            Empty = new Classes.PersonInfo(
+                Emptys[Id].Name,
+                Emptys[Id].Health,
+                Emptys[Id].Armor,
+                Emptys[Id].Level,
+                Emptys[Id].Glasses,
+                Emptys[Id].Money,
+                Emptys[Id].Damage);
+        }
+
         private void AttackPlayer(object sender, System.EventArgs e)
         {
-
+            Player.Health -= Convert.ToInt32(Empty.Damage * 100f / (100f - Player.Armor));
+            UserInfoPlayer();
         }
 
         private void AttackEmpty(object sender, MouseButtonEventArgs e)
         {
-
+            Empty.Health -= Convert.ToInt32(Player.Damage * 100f / (100f - Empty.Armor));
+            if (Empty.Health <= 0)
+            {
+                Player.Glasses += Empty.Glasses;
+                Player.Money -= Empty.Money;
+                UserInfoPlayer();
+                SelectEmpty();
+            }
+            else
+            {
+                emptyHealth.Content = "Жизненные показтели: " + Empty.Health;
+                emptyArmor.Content = "Броня: " + Empty.Armor;
+            }
         }
     }
 }
